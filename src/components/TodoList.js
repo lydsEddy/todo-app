11394969c3f72e6ff0f87e4612e555005e9f6fcb
Lydia Edwards <Lydia@Lydias-MacBook-Air-2.local>
2020-10-15 13:47:04 +0100

    Create form, todoID, todolist

---
+++ src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+    const newTodos = [todo, ...todos];
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>What Is The Plan For Today</h1>
+      <TodoForm onSubmit={addTodo} />
+    </div>
+  );
+}
+
+export default TodoList;
